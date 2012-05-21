5e8bd897dccd76c18c6d6d1a7cf9d3cd18bdc744
adutoit <dutoit.a@gmail.com>
2012-05-21 13:18:57 +0200

    Añado el metodo1 a la clase 1

--- adutoit/src/clase1.java
+++ adutoit/src/clase1.java
@@ -4,7 +4,11 @@ public class clase1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Soy la clase 1");
+		
+	}
 
+	public void metodo1_clase1(){
+		System.out.println("soy el metodo 1 de la clase 1");
 	}
 
 }
